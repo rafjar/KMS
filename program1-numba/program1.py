ed9717f2033ce0c7824517f71199858ac3f68a4f
Rafaello <rafjar@Rafalek>
2020-10-28 14:41:50 +0100

    Poprawiona numba

--- program1-numba/program1.py
+++ program1-numba/program1.py
@@ -1,59 +1,19 @@
 import numpy as np
 from numba import jit
 from tqdm import tqdm
-np.set_printoptions(linewidth=np.inf, precision=3)
-
-save_txt_file = False
-plot_results = False
-
-position_file = open('avs.txt', 'w')
-properties_file = open('properties.txt', 'w')
 
 
-# Funkcja od zapisu kryształu do pliku
-def save_to_file(plik='wyniki.xyz'):
-    with open(plik, 'w') as f:
-        f.write(f'{N}\n\n')
-        for indx, (x, y, z) in enumerate(r):
-            f.write(f'atom{indx}\t{x}\t{y}\t{z}\n')
-
-
-# Funkcja do histogramów pędu
-def plot_momentum():
-    import matplotlib.pyplot as plt
-
-    fig, axs = plt.subplots(1, 3)
-    for (indx, title), ax in zip(enumerate('x y z'.split()), axs):
-        ax.hist(p[:, indx], bins=30)
-        ax.set_title(f'Pęd w osi {title}')
-        ax.grid()
-    plt.show()
-
-
-# Wczytanie warunków początkowych z pliku
+# Wczytanie warunków początkowych
 def load_initial_cond(plik='dane.txt'):
-    global n, m, a, T0, N, k, epsilon, L, f, R, tau, S_o, S_d, S_out, S_xyz
-    '''
-    n - liczba atomów w jednej osi,
-    m - masa atomu,
-    a - stała atomowa,
-    T0 - temperatura początkowa,
-    N - całkowita liczba atomów,
-    k - stała Boltzmanna,
-    epsilon - minimum potencjału,
-    L - promień kuli w której znajduje się gaz,
-    f - stała sprężystości,
-    R - odległość międzyatomowa dla której minimum potencjału,
-    '''
     data = []
+
     with open(plik) as f:
         for line in f.readlines():
             for val in line.split():
                 try:
                     data.append(float(val))
                 except:
-                    pass  # val nie jest liczbą - odrzucenie komentarzy
-
+                    pass
     k = 8.31e-3
     n, m, a, T0, epsilon, L, f, R, tau, S_o, S_d, S_out, S_xyz = data
     n = int(n)
@@ -63,141 +23,122 @@ def load_initial_cond(plik='dane.txt'):
     S_xyz = int(S_xyz)
     N = n**3
 
+    return k, n, N, m, a, T0, epsilon, L, f, R, tau, S_o, S_d, S_out, S_xyz
 
-# Wczytanie war. początkowych
-load_initial_cond()
-
-# Wektory położenia i pędu
-r = np.array([[0, 0, 0] for i in range(N)]).astype(np.float)  # położenia
-p = np.array([[0, 0, 0] for i in range(N)]).astype(np.float)  # pędy
-
-# wektory potrzebne do początkowych pozycji atomów
-b0 = np.array([a, 0, 0])
-b1 = np.array([a/2, a*np.sqrt(3)/2, 0])
-b2 = np.array([a/2, a*np.sqrt(3)/6, a*np.sqrt(2/3)])
 
 # Ustawienie atomów w siatce rombu
-for i0 in range(n):
-    for i1 in range(n):
-        for i2 in range(n):
-            i = i0 + i1*n + i2*n**2
-            r[i, :] = (i0-(n-1)/2)*b0 + (i1-(n-1)/2)*b1 + (i2-(n-1)/2)*b2
+def set_positions(r_org, n, b0, b1, b2):
+    r = np.zeros(r_org.shape)
+    for i0 in range(n):
+        for i1 in range(n):
+            for i2 in range(n):
+                i = i0 + i1*n + i2*n**2
+                r[i, :] = (i0-(n-1)/2)*b0 + (i1-(n-1)/2)*b1 + (i2-(n-1)/2)*b2
+    return r
 
-if save_txt_file:
-    save_to_file()
 
 # Losowanie pędów
-random_01 = np.random.random(size=(N, 3))
-random_01[random_01 == 0] = 1
-E_kin = np.array(-1/2 * k * T0 * np.log(random_01))
-for indx, (x, y, z) in enumerate(np.random.choice((-1, 1), size=(N, 3))):
-    p[indx, :] = x*np.sqrt(2*m*E_kin[indx, 0]), y*np.sqrt(2*m*E_kin[indx, 1]), z*np.sqrt(2*m*E_kin[indx, 2])
+def randomize_momentum(N, k, T0, m):
+    p = np.array([[0, 0, 0] for i in range(N)]).astype(np.float)
+    random_01 = np.random.random(size=(N, 3))
+    random_01[random_01 == 0] = 1
+    E_kin = np.array(-1/2 * k * T0 * np.log(random_01))
+    for indx, (x, y, z) in enumerate(np.random.choice((-1, 1), size=(N, 3))):
+        p[indx, :] = x*np.sqrt(2*m*E_kin[indx, 0]), y*np.sqrt(2*m*E_kin[indx, 1]), z*np.sqrt(2*m*E_kin[indx, 2])
 
-p = p - sum(p)/N  # wyeliminowanie ruchu środka masy
-
-if plot_results:
-    plot_momentum()
-
-# Obliczenie potencjału i sił
-Vs = np.zeros(N)
-Vp = np.zeros((N, N))
-Fs = np.zeros((N, 3))
-Fp = np.zeros((N, N, 3))
-V = None
-F = None
-P = None
-T = None
-H = None
-T_avg = 0
-P_avg = 0
-H_avg = 0
-t = 0
+    return p - sum(p)/N  # wyeliminowanie ruchu środka masy
 
 
-# Obliczenie sił i potencjałów
-@jit
-def count_forces(Vs, Vp, Fs, Fp, r):
-    Fp[:] = 0
+# Liczenie sił i potencjałów
+@jit(nopython=True)
+def count_FV(N, r, L, f, epsilon, R):
+    Vs = np.zeros(N)
+    Vp = np.zeros(N)
+    Fs = np.zeros((N, 3))
+    Fp = np.zeros((N, 3))
+
     for indx1, particle1 in enumerate(r):
-
         r_abs = np.linalg.norm(particle1)
-        if r_abs >= L:
-            Vs[indx1] = 1/2 * f * (r_abs-L)**2
-            Fs[indx1] = f*(L-r_abs)*particle1/r_abs
+
+        if r_abs > L:
+            Vs[indx1] = f/2 * (r_abs - L)**2
+            Fs[indx1] = f*(L-r_abs) * particle1/r_abs
 
         for indx2, particle2 in enumerate(r[:indx1]):
             r_abs = np.linalg.norm(particle1 - particle2)
-            Vp[indx1, indx2] = epsilon * ((R / r_abs)**12 - 2*(R/r_abs)**6)
-            Fp[indx1, indx2] = 12*epsilon * ((R/r_abs)**12 - (R/r_abs)**6) * ((particle1-particle2)/r_abs**2)
-            Fp[indx2, indx1] = -Fp[indx1, indx2]
+            Vp[indx1] += epsilon * ((R/r_abs)**12 - 2*(R/r_abs)**6)
+            F = 12*epsilon * ((R/r_abs)**12 - (R/r_abs)**6) * (particle1 - particle2)/r_abs**2
+            Fp[indx1] += F
+            Fp[indx2] -= F
 
-    return np.sum(Vs) + np.sum(Vp), Fs + np.sum(Fp, axis=1)
+    return np.sum(Vs) + np.sum(Vp), Fs + Fp, Fs
 
 
-# Obliczanie ciśnienia
-def count_pressure():
-    global Fs, L, P
-    P = 1/(4*np.pi*L**2) * np.sum(np.linalg.norm(Fs, axis=1))
+# Obliczenie ciśnienia
+def count_pressure(Fs, L):
+    P = np.sum(np.linalg.norm(Fs, axis=-1)) / (4 * np.pi * L**2)
+    return P
 
 
 # Obliczenie energii
-def count_energy():
-    global E_kin
-    E_kin = np.linalg.norm(p, axis=1)**2 / (2*m)
+def count_energy(p, m):
+    E_kin = np.linalg.norm(p, axis=-1)**2 / (2*m)
+    return E_kin
 
 
-# Obliczanie temperatury
-def count_temperature():
-    global T, p, N, k, m
-    count_energy()
+# Obliczenie temperatury
+def count_temperature(p, m, N, k, E_kin):
     T = 2/(3*N*k) * np.sum(E_kin)
+    return T
 
 
-# Obliczanie Hamiltonianu
-def count_hamiltonian():
-    global p, V, H, V
-    H = np.sum(np.linalg.norm(p, axis=1)**2 / (2*m)) + V
+# Obliczenie Hamiltonianiu
+def count_hamiltonian(p, m, V):
+    H = np.sum(np.linalg.norm(p, axis=-1)**2 / (2*m)) + V
+    return H
 
 
-# Całkowanie równania ruchu
-def integrate():
-    global p, F, r, tau, t, F, V
-    p = p + F*tau/2
-    r = r + p*tau/m
-    V, F = count_forces(Vs, Vp, Fs, Fp, r)
-    count_pressure()
-    p = p + F*tau/2
+# Całkowanie równiania ruchu
+def integrate(p, F, tau, m, N, r, L, f, epsilon, R, t):
+    p += F*tau/2
+    r += p*tau/m
+    V, F, Fs = count_FV(N, r, L, f, epsilon, R)
+    P = count_pressure(Fs, L)
+    p += F*tau/2
     t += tau
+
+    return t, V, F, P, r, p
 
 
 # Zapisanie parametrów układu
-def save_properties():
-    global H, V, T, P, t
+def save_properties(properties_file, t, H, V, T, P):
     properties_file.write(f'{t}\t{H}\t{V}\t{T}\t{P}\n')
 
 
 # Zapisanie położeń atomów
-def save_positions():
-    global r, E_kin, N
+def save_positions(position_file, N, r, E_kin):
     position_file.write(f'{N}\n\n')
     for (x, y, z), E in zip(r, E_kin):
         position_file.write(f'Ar\t{x}\t{y}\t{z}\t{E}\n')
 
 
 # Symulacja całego układu
-def symulacja():
-    global S_o, S_d, S_out, S_xyz, T_avg, H_avg, P_avg
+def symulacja(S_o, S_d, p, F, tau, m, N, r, L, f, epsilon, R, t, k, S_out, S_xyz, properties_file, position_file):
+    T_avg = 0.
+    P_avg = 0.
+    H_avg = 0.
 
     for i in tqdm(range(S_o + S_d)):
-        integrate()
-        count_temperature()
-        count_hamiltonian()
+        t, V, F, P, r, p = integrate(p, F, tau, m, N, r, L, f, epsilon, R, t)
+        E_kin = count_energy(p, m)
+        T = count_temperature(p, m, N, k, E_kin)
+        H = count_hamiltonian(p, m, V)
 
         if not i % S_out:
-            save_properties()
+            save_properties(properties_file, t, H, V, T, P)
 
         if not i % S_xyz:
-            save_positions()
+            save_positions(position_file, N, r, E_kin)
 
         if i >= S_o:
             T_avg += T
@@ -208,16 +149,39 @@ def symulacja():
     P_avg /= S_d
     H_avg /= S_d
 
+    return T_avg, P_avg, H_avg
 
-# Wstępne obliczenie sił i uruchomienie symulacji
-V, F = count_forces(Vs, Vp, Fs, Fp, r)
-symulacja()
 
-position_file.close()
-properties_file.close()
+def main():
+    position_file = open('avs.txt', 'w')
+    properties_file = open('properties.txt', 'w')
 
-# Zapisanie wartości średnich
-with open('mean_properties.txt', 'w') as f:
-    f.write(f'T_avg:\t{T_avg}\n')
-    f.write(f'P_avg:\t{P_avg}\n')
-    f.write(f'H_avg:\t{H_avg}\n')
+    # Wczytanie warunków początkowych
+    k, n, N, m, a, T0, epsilon, L, f, R, tau, S_o, S_d, S_out, S_xyz = load_initial_cond()
+
+    # Wektory położenia i pędu
+    r = np.array([[0, 0, 0] for i in range(N)]).astype(np.float)  # położenia
+    p = np.array([[0, 0, 0] for i in range(N)]).astype(np.float)  # pędy
+
+    # wektory potrzebne do początkowych pozycji atomów
+    b0 = np.array([a, 0, 0])
+    b1 = np.array([a/2, a*np.sqrt(3)/2, 0])
+    b2 = np.array([a/2, a*np.sqrt(3)/6, a*np.sqrt(2/3)])
+
+    # Ustawienie położeń początkowych
+    r = set_positions(r, n, b0, b1, b2)
+    t = 0.
+
+    # Losowanie pędów
+    p = randomize_momentum(N, k, T0, m)
+
+    V, F, Fs = count_FV(N, r, L, f, epsilon, R)
+
+    T_avg, P_avg, H_avg = symulacja(S_o, S_d, p, F, tau, m, N, r, L, f, epsilon, R, t, k, S_out, S_xyz, properties_file, position_file)
+
+    position_file.close()
+    properties_file.close()
+
+
+if __name__ == '__main__':
+    main()
