5d8ea4131557301ec46047986c53d2a8b3f0f1d3
Rafaello <rafjar@Rafalek>
2020-11-18 11:39:43 +0100

    Rozdział na funkcje

--- program2/main.cpp
+++ program2/main.cpp
@@ -4,6 +4,13 @@
 #include <string>
 #include <iostream>
 
+inline double count_H (const std::vector<double> &psi, const int &k, const double &delta_x, const double &kappa, const double &omega, const double &tau) {
+    if(k == 0 || k == (int)psi.size()-1)
+        return 0;
+    return -0.5 * ((psi[k-1] - 2*psi[k] + psi[k+1]) / (delta_x*delta_x)) 
+           + kappa * (k*delta_x - 0.5) * psi[k] * std::sin(omega * tau);
+}
+
 int main() {
     std::ifstream dane("dane.txt"); // Plik z danymi początkowymi
     std::ofstream gestosc_prawd("gestosc_prawd.dat"); // Otworzenie pliku do zapisu gęstości prawdopodobieństwa
@@ -40,11 +47,9 @@ int main() {
 
     // Inicjalizacja Hamiltonianów
     for(int k=1; k<=N-1; ++k) {
-        H_R[k] = -1./2 * ((psi_R[k-1] - 2*psi_R[k] + psi_R[k+1]) / (delta_x*delta_x))
-                + kappa * (k*delta_x - 1./2) * psi_R[k] * std::sin(omega * tau);
+        H_R[k] = count_H(psi_R, k, delta_x, kappa, omega, tau);
 
-        H_I[k] = -1./2 * ((psi_I[k-1] - 2*psi_I[k] + psi_I[k+1]) / (delta_x*delta_x))
-                + kappa * (k*delta_x - 1./2) * psi_I[k] * std::sin(omega * tau);
+        H_I[k] = count_H(psi_I, k, delta_x, kappa, omega, tau);
     }
 
     // Pętla symulacji
@@ -55,21 +60,13 @@ int main() {
 
             tau += delta_tau/2;
             // Obliczenie H_R w czasie tau + d_tau/2
-            if(k == 0 || k == N)
-                H_R[k] = 0;
-            else
-                H_R[k] = -1./2 * ((psi_R[k-1] - 2*psi_R[k] + psi_R[k+1]) / (delta_x*delta_x))
-                        + kappa * (k*delta_x - 1./2) * psi_R[k] * std::sin(omega * tau);
+            H_R[k] = count_H(psi_R, k, delta_x, kappa, omega, tau);
 
             // Obliczenie psi_I w czasie tau + d_tau
             psi_I[k] -= H_R[k] * delta_tau;
 
             // Obliczenie H_I w czasie tau + d_tau/2
-            if(k == 0 || k == N)
-                H_I[k] = 0;
-            else
-                H_I[k] = -1./2 * ((psi_I[k-1] - 2*psi_I[k] + psi_I[k+1]) / (delta_x*delta_x))
-                        + kappa * (k*delta_x - 1./2) * psi_I[k] * std::sin(omega * tau);
+            H_I[k] = count_H(psi_I, k, delta_x, kappa, omega, tau);
 
             tau += delta_tau/2;
             // Obliczenie psi_R w czasie tau + d_tau
@@ -87,8 +84,7 @@ int main() {
         if(!(i%zapis_parametrow)) {
             // Obliczenie H_R w czasie tau
             for(int k=1; k<=N-1; ++k) 
-                H_R[k] = -1./2 * ((psi_R[k-1] - 2*psi_R[k] + psi_R[k+1]) / (delta_x*delta_x))
-                        + kappa * (k*delta_x - 1./2) * psi_R[k] * std::sin(omega * tau);
+                H_R[k] = count_H(psi_R, k, delta_x, kappa, omega, tau);
 
             double N_zapis = 0, x_zapis = 0, E_zapis = 0;
             for(int k=0; k<=N; ++k) {
